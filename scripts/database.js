efde31aed0b216b031555cf4cd98e9fa7fd4a7b0
Austin Davis <davis.austin1.1.c@gmail.com>
2023-03-07 15:57:42 -0500

    Tour some sugar on me boilerplate w/basic database framework

---
+++ scripts/database.js
@@ -0,0 +1,171 @@
+
+const database = {
+    bookings: [{
+        id:
+        bandId: 
+        venueId:
+        dateOfBooking:
+    }, {
+        id:
+        bandId: 
+        venueId:
+        dateOfBooking:
+    }, {
+        id:
+        bandId: 
+        venueId:
+        dateOfBooking:
+    }, {
+        id:
+        bandId: 
+        venueId:
+        dateOfBooking:
+    }, {
+        id:
+        bandId: 
+        venueId:
+        dateOfBooking:
+    }, {
+        id:
+        bandId: 
+        venueId:
+        dateOfBooking:
+    }, {
+        id:
+        bandId: 
+        venueId:
+        dateOfBooking:
+    }, {
+        id:
+        bandId: 
+        venueId:
+        dateOfBooking:
+    }, {
+        id:
+        bandId: 
+        venueId:
+        dateOfBooking:
+    }, {
+        id:
+        bandId: 
+        venueId:
+        dateOfBooking:
+    }, {
+        id:
+        bandId: 
+        venueId:
+        dateOfBooking:
+    }, {
+        id:
+        bandId: 
+        venueId:
+        dateOfBooking:
+    }
+    ],
+    venues: [{
+        id:
+        name:
+        address:
+        squareFootage:
+        maxOccupancy:
+    }, {
+        id:
+        name:
+        address:
+        squareFootage:
+        maxOccupancy:
+    }, {
+        id:
+        name:
+        address:
+        squareFootage:
+        maxOccupancy:
+    }, {
+        id:
+        name:
+        address:
+        squareFootage:
+        maxOccupancy:
+    }, {
+        id:
+        name:
+        address:
+        squareFootage:
+        maxOccupancy:
+    }, {
+        id:
+        name:
+        address:
+        squareFootage:
+        maxOccupancy:
+    }, {
+        id:
+        name:
+        address:
+        squareFootage:
+        maxOccupancy:
+    }, {
+        id:
+        name:
+        address:
+        squareFootage:
+        maxOccupancy:
+    }, {
+        id:
+        name:
+        address:
+        squareFootage:
+        maxOccupancy:
+    }, 
+    ],
+    bands: [{
+        id:
+        name:
+        numOfMembers:
+        genre:
+        yearFormed:
+    }, {
+        id:
+        name:
+        numOfMembers:
+        genre:
+        yearFormed:
+    }, {
+        id:
+        name:
+        numOfMembers:
+        genre:
+        yearFormed:
+    }, {
+        id:
+        name:
+        numOfMembers:
+        genre:
+        yearFormed:
+    }, {
+        id:
+        name:
+        numOfMembers:
+        genre:
+        yearFormed:
+    }, {
+        id:
+        name:
+        numOfMembers:
+        genre:
+        yearFormed:
+    }, {
+        id:
+        name:
+        numOfMembers:
+        genre:
+        yearFormed:
+    }, {
+        id:
+        name:
+        numOfMembers:
+        genre:
+        yearFormed:
+    }, 
+    ]
+}
